a8ed099656f7f4d6dc0c2d19970d2fd6abd3f942
Marco Venturini <venturini.marco.mail@gmail.com>
2026-02-28 22:58:11 +0100

    Part 5b exercises (bloglist-frontend): 5.7 button to view blog details

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,36 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [ isDetailMode, setIsDetailMode ] = useState(false)
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+  const hideView = () => setIsDetailMode(!isDetailMode)
+  
+  return (
+  <div style={blogStyle}>
+    {isDetailMode ? 
+      <div>
+        <p>{blog.title}</p>
+        <p>{blog.author}</p>
+        <p>{blog.url}</p>
+        <p>
+          likes {blog.likes}
+          <button>like</button>
+        </p>
+        <button onClick={hideView}> hide</button>
+      </div>
+      :
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={hideView}> view</button>
+      </div>
+    }
   </div>  
-)
+)}
 
 export default Blog
